bab5c1a3bc21b8ebbe6ffe362cd27bf88239cf0b
tafeng.dxx <tafeng.dxx@taobao.com>
2013-03-03 15:23:47 +0800

    update

--- app/controller/Orders.js
+++ app/controller/Orders.js
@@ -8,8 +8,8 @@
 Ext.define('AM.controller.Orders', {
     extend:'Ext.app.Controller',
     views:[
-      /*  'order.List',
-        'order.DetailList',*/
+        /*  'order.List',
+         'order.DetailList',*/
         'order.Detail',
         'order.General',
         'order.Audit'
@@ -24,13 +24,13 @@ Ext.define('AM.controller.Orders', {
 
     refs:[
         {
-            ref: 'panel',
-            selector: 'orderpanel'
+            ref:'panel',
+            selector:'orderpanel'
         },
-       /* {
-            ref: 'orderGeneral',
-            selector: 'ordergeneral'
-        },*/
+        /* {
+         ref: 'orderGeneral',
+         selector: 'ordergeneral'
+         },*/
         {
             ref:'list',
             selector:'orderlist'
@@ -49,50 +49,48 @@ Ext.define('AM.controller.Orders', {
         }
     ],
 
-    checkEnable: function(sm) {
+    checkEnable:function (sm) {
         var len = sm.getSelection().length;
         this.getEditButton().setDisabled(len !== 1);
         this.getViewButton().setDisabled(len !== 1);
     },
 
-    saveOrder: function(btn, isNew) {
-       var detailPanel =  btn.up('orderdetail'),
+    saveOrder:function (btn, isNew) {
+        var detailPanel = btn.up('orderdetail'),
             form = deaitlPanel.down('orderform'),
             list = detailPanel.down('order_detail_list');
 
 
-
     },
     init:function () {
-         this.control({
-             'orderlist': {
-                 'selectionchange': this.checkEnable
-             },
-             '#J_OrderAdd': {
-                 'click': function() {
-                     var tab = Ext.widget('orderdetail', {
-                         title: '新增订单'
-                     });
+        this.control({
+            'orderlist':{
+                'selectionchange':this.checkEnable
+            },
+            '#J_OrderAdd':{
+                'click':function () {
+                    var tab = Ext.widget('orderdetail', {
+                        title:'新增订单'
+                    });
 
-                     var panel = this.getPanel();
-                     panel.add(tab);
-                     panel.setActiveTab(tab);
-                 }
-             },
-             'orderdetail button[action=add_save]': {
-                 'click':  function(btn) {
-                     var detailPanel =  btn.up('orderdetail'),
-                         form = detailPanel.down('orderform'),
-                         list = detailPanel.down('order_detail_list'),
-                         record = Ext.create('AM.model.Order');
+                    var panel = this.getPanel();
+                    panel.add(tab);
+                    panel.setActiveTab(tab);
+                }
+            },
+            'orderdetail button[action=add_save]':{
+                'click':function (btn) {
+                    var detailPanel = btn.up('orderdetail'),
+                        form = detailPanel.down('orderform'),
+                        list = detailPanel.down('order_detail_list'),
+                        record = Ext.create('AM.model.Order');
 
-
-                     record.set(form.getForm().getFieldValues());
-                     record.detail().add( list.getStore().getNewRecords());
-                     this.getStore('Orders').add(record);
-                     this.application.sync(this.getStore('Orders'), this)
-                 }
-             }
-         });
+                    record.set(form.getForm().getFieldValues());
+                    record.detail().add(list.getStore().getNewRecords());
+                    this.getStore('Orders').add(record);
+                    this.application.sync(this.getStore('Orders'), this);
+                }
+            }
+        });
     }
 });
