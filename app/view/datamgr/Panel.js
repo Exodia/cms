5d98088567ca273a90fe9f4e77e1aa63d58e61af
tafeng.dxx <tafeng.dxx@taobao.com>
2013-03-08 19:21:46 +0800

    update

--- app/view/datamgr/Panel.js
+++ app/view/datamgr/Panel.js
@@ -1,6 +1,50 @@
 Ext.define('AM.view.datamgr.Panel', {
-    extend:'Ext.panel.Panel',
-    alias:'widget.datamgrpanel',
-    title:'数据管理',
-    items:[]
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.datamgrpanel',
+    title: '数据管理',
+    layout: {
+        type: 'vbox',
+        align: 'center'
+    },
+    items: [
+        {
+            xtype: 'form',
+            title: '导入数据',
+            width: 400,
+            bodyPadding: 10,
+            frame: true,
+            draggable: true,
+            margin: '200',
+            items: [
+                {
+                    xtype: 'filefield',
+                    name: 'data',
+                    fieldLabel: '数据表路径',
+                    labelWidth: 80,
+                    msgTarget: 'side',
+                    allowBlank: false,
+                    anchor: '100%',
+                    buttonText: '选择文件'
+                }
+            ],
+
+            buttons: [
+                {
+                    text: '导入',
+                    handler: function () {
+                        var form = this.up('form').getForm();
+                        if (form.isValid()) {
+                            form.submit({
+                                url: AM.API['dataImport'],
+                                waitMsg: '正在导入!',
+                                success: function (fp, o) {
+                                    Ext.Msg.alert('成功', '文件' + o.result.file + '已导入！ ');
+                                }
+                            });
+                        }
+                    }
+                }
+            ]
+        }
+    ]
 });
