a7e59a37e86af27bdad677dfc99e37ee83618060
Exodia <d_xinxin@163.com>
2013-03-08 17:15:28 +0800

    add login

--- app/controller/Orders.js
+++ app/controller/Orders.js
@@ -1,54 +1,54 @@
 Ext.define('AM.controller.Orders', {
-    extend:'Ext.app.Controller',
-    views:[
-      /*  'order.List',
-        'order.DetailList',*/
+    extend: 'Ext.app.Controller',
+    views: [
+        /*  'order.List',
+         'order.DetailList',*/
         'order.Detail',
         'order.General',
         'order.Audit'
     ],
-    stores:[
+    stores: [
         'Materials',
         'Customs',
         'SalesMen',
         'OrderDetails',
         'Orders'
     ],
-    models:['Custom', 'Order'],
+    models: ['Custom', 'Order'],
 
-    refs:[
+    refs: [
         {
             ref: 'panel',
             selector: 'orderpanel'
         },
-       /* {
-            ref: 'orderGeneral',
-            selector: 'ordergeneral'
-        },*/
+        /* {
+         ref: 'orderGeneral',
+         selector: 'ordergeneral'
+         },*/
         {
-            ref:'list',
-            selector:'orderlist'
+            ref: 'list',
+            selector: 'orderlist'
         },
         {
-            ref:'editButton',
-            selector:'#J_OrderEdit'
+            ref: 'editButton',
+            selector: '#J_OrderEdit'
         },
         {
-            ref:'addButton',
-            selector:'#J_OrderAdd'
+            ref: 'addButton',
+            selector: '#J_OrderAdd'
         },
         {
-            ref:'viewButton',
-            selector:'#J_OrderView'
+            ref: 'viewButton',
+            selector: '#J_OrderView'
         }
     ],
 
-    checkEnable: function(sm) {
+    checkEnable: function (sm) {
         var len = sm.getSelection().length;
         this.getEditButton().setDisabled(len !== 1);
         this.getViewButton().setDisabled(len !== 1);
     },
-    addOrder: function() {
+    addOrder: function () {
         var tab = Ext.widget('orderdetail', {
             title: '新增订单',
             order: this.getStore('Orders').add({})[0],
@@ -59,14 +59,14 @@ Ext.define('AM.controller.Orders', {
         panel.add(tab);
         panel.setActiveTab(tab);
     },
-    saveOrder: function(btn) {
-       var form = btn.up('orderdetail').down('orderform'),
-           record = form.order,
-           values = form.getValues();
-       record.set(values);
-       this.application.sync(this.getStore('Orders'), this);
+    saveOrder: function (btn) {
+        var form = btn.up('orderdetail').down('orderform'),
+            record = form.order,
+            values = form.getValues();
+        record.set(values);
+        this.application.sync(this.getStore('Orders'), this);
     },
-    viewOrder: function() {
+    viewOrder: function () {
         var tab = Ext.widget('orderdetail', {
             order: this.getList().getSelectionModel().getSelection()[0],
             orderStatus: 'view'
@@ -83,39 +83,43 @@ Ext.define('AM.controller.Orders', {
         form.down('custom').setValue(data.custom.id);
         form.down('salesman').setValue(data.salesman.id);
     },
-    printOrder: function(btn) {
+    printOrder: function (btn) {
         var order = btn.up('orderdetail').order,
             data = order.getData(),
             detail = order.detail(),
             tpl = document.getElementById('printTpl').innerHTML;
 
         data.detail = [];
-        detail.each(function(rec) {
-           data.detail.push(rec.getData());
+        detail.each(function (rec) {
+            var recData = rec.getData();
+            recData.deadline = Ext.Date.format(recData.deadline, 'Y年m月d日');
+            data.detail.push(recData);
         });
-        console.log(data);
+
+        data.date = Ext.Date.format(data.date, 'Y年m月d日');
+
         var t = new Ext.XTemplate(tpl);
         var w = window.open('');
         w.document.write(t.apply(data));
 //        w.print();
     },
-    init:function () {
-         this.control({
-             'orderlist': {
-                 'selectionchange': this.checkEnable
-             },
-             '#J_OrderAdd': {
-                 'click': this.addOrder
-             },
-             'orderdetail button[action=add_save]': {
-                 'click': this.saveOrder
-             },
-             'orderdetail button[action=print]': {
-                 'click': this.printOrder
-             },
-             '#J_OrderView': {
-                 'click': this.viewOrder
-             }
-         });
+    init: function () {
+        this.control({
+            'orderlist': {
+                'selectionchange': this.checkEnable
+            },
+            '#J_OrderAdd': {
+                'click': this.addOrder
+            },
+            'orderdetail button[action=add_save]': {
+                'click': this.saveOrder
+            },
+            'orderdetail button[action=print]': {
+                'click': this.printOrder
+            },
+            '#J_OrderView': {
+                'click': this.viewOrder
+            }
+        });
     }
 });
