bc2aee1abf629fdc4a40a7318299ec1cb0e8a718
Exodia <d_xinxin@163.com>
2013-03-09 00:11:10 +0800

    update

--- app/view/order/Form.js
+++ app/view/order/Form.js
@@ -1,73 +1,75 @@
 Ext.define('AM.view.order.Form', {
-    extend:'Ext.form.Panel',
-    requires:['AM.view.SaleGroup', 'AM.view.Custom', 'AM.view.SalesMan'],
-    alias:'widget.orderform',
-    layout:'border',
-    collapsible:true,
-    frame:true,
-    region:'west',
-    layout:'vbox',
-    padding:15,
-    autoScroll:true,
-    defaultType:'textfield',
+    extend: 'Ext.form.Panel',
+    requires: ['AM.view.SaleGroup', 'AM.view.Custom', 'AM.view.SalesMan'],
+    alias: 'widget.orderform',
+    layout: 'border',
+    collapsible: true,
+    frame: true,
+    region: 'west',
+    layout: 'vbox',
+    padding: 15,
+    autoScroll: true,
+    defaultType: 'textfield',
 
-    width:200,
-    initComponent:function () {
+    width: 200,
+    initComponent: function () {
         this.defaults = {
-            labelAlign:'top',
-            margin:'15 0',
-            width:150,
-            readOnly: this.orderStatus === 'view'
+            labelAlign: 'top',
+            margin: '15 0',
+            width: 150,
+            disabled: this.orderStatus !== 'add'
         };
+
 
         this.items = [
             {
-                fieldLabel:'项目号',
-                name:'project',
-                allowBlank:false
+                fieldLabel: '项目号',
+                name: 'project',
+                allowBlank: false
             },
             {
-                fieldLabel:'订单日期',
-                name:'date',
-                format:'Y年m月d日',
+                fieldLabel: '订单日期',
+                name: 'date',
+                format: 'Y年m月d日',
                 submitFormat: 'Y-m-d',
-                xtype:'datefield'
+                xtype: 'datefield'
             },
             {
-                xtype:'custom'
+                xtype: 'custom',
+                readOnly: this.orderStatus !== 'add'
             },
             {
-                fieldLabel:'销售组',
+                fieldLabel: '销售组',
                 readOnly: true,
                 disabled: true,
-                width:150,
-                padding:0,
-                name:'saleGroup',
-                xtype:'textfield'
+                width: 150,
+                padding: 0,
+                name: 'saleGroup',
+                xtype: 'textfield'
             },
             {
-                fieldLabel:'销售员',
+                fieldLabel: '销售员',
                 readOnly: true,
                 disabled: true,
-                name:'salesManName',
-                xtype:'textfield'
+                name: 'salesManName',
+                xtype: 'textfield'
             },
             {
-                name:'customId',
-                xtype:'textfield',
+                name: 'customId',
+                xtype: 'textfield',
                 inputType: 'hidden'
             },
             {
-                name:'salesManId',
-                xtype:'textfield',
+                name: 'salesManId',
+                xtype: 'textfield',
                 inputType: 'hidden'
             }
         ];
 
         this.orderStatus === 'view' && this.items.unshift({
-            fieldLabel:'订单编号',
+            fieldLabel: '订单编号',
             disabled: true,
-            name:'id'
+            name: 'id'
         });
 
         this.callParent(arguments);
