22bbdf3468ebbcbcad8afab468419383d15d663d
tafeng.dxx <tafeng.dxx@taobao.com>
2013-03-24 15:34:19 +0800

    update

--- app/controller/Transports.js
+++ app/controller/Transports.js
@@ -21,25 +21,22 @@ Ext.define('AM.controller.Transports', {
             selector:'#J_TransportAdd'
         }
     ],
+
     init:function () {
         this.control({
             '#J_TransportAdd':{
                 click:function () {
                     Ext.widget('transportedit', {
-                        title: '新增物流信息'
+                        title: '新增发运信息'
                     });
                 }
             },
 
-            '#J_TransportEdit':{
-                click:this.editTransport
+            '#J_TransportConfirm':{
+                click:this.confirmTransport
             },
 
-            '#J_TransportDelete':{
-                click:this.delTransport
-            },
-
-            'transportlist':{
+            'transport_general transport_list':{
                 selectionchange:this.checkEnable
             },
 
@@ -48,42 +45,9 @@ Ext.define('AM.controller.Transports', {
             }
         });
     },
-    checkEnable:function (sm) {
-        var len = sm.getSelection().length;
-        this.getEditButton().setDisabled(len !== 1);
-        this.getDelButton().setDisabled(len == 0);
-    },
-
-    delTransport:function () {
-        this.application.confirm('注意', '确认要删除所选信息？', function (btnId) {
-            if (btnId == 'ok') {
-                var sm = this.getList().getSelectionModel(),
-                    store = this.getTransportsStore();
-                store.remove(sm.getSelection());
-                this.application.sync(store, this);
-            }
-        }, this);
-    },
-
-    updateTransport:function (button) {
-        var win = button.up('window'),
-            form = win.down('form'),
-            values = form.getValues(),
-            record = form.getRecord(),
-            store = this.getTransportsStore();
-
-        if (!form.getForm().isValid()) {
-            Ext.Msg.alert('错误', '数据非法，请重新填写！');
-            return;
-        }
-        record ? record.set(values) : store.add(values);
-        win.close();
-        this.application.sync(store, this);
-    },
-
-    editTransport:function () {
-        var view = Ext.widget('transportedit'),
-            record = this.getList().getSelectionModel().getSelection()[0];
-        view.down('form').loadRecord(record);
+    checkEnable:function (sm, rec) {
+        var len = rec.length;
+        this.getConfirmButton().setDisabled(len !== 1 || rec[0].get('status') === 1);
     }
+
 });
