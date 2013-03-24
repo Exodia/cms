aba0c4d70d75621496fceee007baf20f161c7fb2
Exodia <d_xinxin@163.com>
2013-03-24 14:32:26 +0800

    add login

--- app/view/transport/Panel.js
+++ app/view/transport/Panel.js
@@ -1,8 +1,8 @@
 Ext.define('AM.view.transport.Panel', {
     extend:'Ext.panel.Panel',
-    alias:'widget.transportpanel',
+    alias:'widget.transport_panel',
     title:'发运管理',
     items:[{
-        xtype:'transportlist'
+        xtype:'transport_list'
     }]
 });
