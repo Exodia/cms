44d176925bf97bdee84670ce4c6137eac1d581ca
Exodia <d_xinxin@163.com>
2013-04-14 18:13:18 +0800

    add login

--- app/view/datamgr/Panel.js
+++ app/view/datamgr/Panel.js
@@ -9,49 +9,52 @@ Ext.define('AM.view.datamgr.Panel', {
         },
 
         {
+            title: '导入数据',
             layout: {
                 type: 'vbox',
                 align: 'center'
             },
-            xtype: 'form',
-            title: '导入数据',
-            width: 300,
-            height: 200,
-            bodyPadding: 10,
-            frame: true,
-            draggable: true,
-            fileUpload: true,
-            margin: '100',
-            items: [
-                {
-                    xtype: 'filefield',
-                    name: 'data',
-                    fieldLabel: '数据表路径',
-                    labelWidth: 80,
-                    msgTarget: 'side',
-                    allowBlank: false,
-                    anchor: '100%',
-                    buttonText: '选择文件'
-                }
-            ],
+            items: {
+                xtype: 'form',
+                width: 400,
+                bodyPadding: 10,
+                frame: true,
+                draggable: true,
+                fileUpload: true,
+                margin: '100',
+                items: [
+                    {
+                        xtype: 'filefield',
+                        name: 'data',
+                        fieldLabel: '数据表路径',
+                        labelWidth: 80,
+                        msgTarget: 'side',
+                        allowBlank: false,
+                        anchor: '100%',
+                        buttonText: '选择文件'
+                    }
+                ],
 
-            buttons: [
-                {
-                    text: '导入',
-                    handler: function () {
-                        var form = this.up('form').getForm();
-                        if (form.isValid()) {
-                            form.submit({
-                                url: AM.API['dataImport'],
-                                waitMsg: '正在导入!',
-                                success: function (fp, o) {
-                                    Ext.Msg.alert('成功', '文件已导入！');
-                                }
-                            });
+                buttons: [
+                    {
+                        text: '导入',
+                        handler: function () {
+                            var form = this.up('form').getForm();
+                            if (form.isValid()) {
+                                form.submit({
+                                    url: AM.API['dataImport'],
+                                    waitMsg: '正在导入!',
+                                    success: function (fp, o) {
+                                        Ext.Msg.alert('成功', '文件已导入！');
+                                    }
+                                });
+                            }
                         }
                     }
-                }
-            ]
+                ]
+            }
+
+
         }
     ]
 });
